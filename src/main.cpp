5090f1dc4830731702074021044e86241d6cf859
Ali <ali@ali.ma>
2022-04-02 11:49:37 +0000

    cleaning main

--- src/main.cpp
+++ src/main.cpp
@@ -10,16 +10,10 @@
 #define DEBUG false
 int main() {
   System system;
-   if(!DEBUG){
-    NCursesDisplay::Display(system);
-  } else{
-    for(int i = 0; i< 99 ; i++){
-    std::cout << "CPU :"    <<system.Cpu().Utilization() <<std::endl;
-    std::cout << "idle : "  <<(system.Cpu().Tic().kIdle + system.Cpu().Toc().kUser)
-    << " Non Idle: " <<system.Cpu().Toc().kNonIdle() << std::endl;
-    }
-   
-      
+   if(DEBUG){
+    
+  } else{ 
+      NCursesDisplay::Display(system);
   }
    
 }
